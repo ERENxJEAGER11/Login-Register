b206d8951ce5a5aa5b5fa899d478f8032f71ff21
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2023-01-03 13:43:57 +0530

    state management
    state for login and register is manages in this change , almost front end part is complited

--- src/Components/login/login.js
+++ src/Components/login/login.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./login.css"
 const Login = () => {
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    reEnterPassword: "",
+ })
+
+ const handleChange = e => {
+   // console.log(e.target);
+   const { name, value } = e.target;
+   setUser({
+     ...user,
+     [name]: value
+   })
+ }
+
   return (
     <div className="login">
         <h1>Login</h1>
-        <input type="text" placeholder="Enter Email"></input>
-        <input type="password" placeholder="Enter password"></input>
+        <input type="text" name="email" value={user.email} placeholder="Enter Email" onChange={handleChange}></input>
+        <input type="password" name="password" value={user.password} placeholder="Enter Password" onChange={handleChange}></input>
     <div className="button">Login</div>
     <div>or</div>
     <div className="button">Register</div>
--- src/Components/register/register.js
+++ src/Components/register/register.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./register.css"
  const Register = () => {
+
+  const [user, setUser] = useState({
+     name: "",
+     email: "",
+     password: "",
+     reEnterPassword: "",
+  })
+
+  const handleChange = e => {
+    // console.log(e.target);
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value
+    })
+  }
   return (
     <div className="register">
         <h1>Register</h1>
-        <input type="text" placeholder="Enter Name"></input>
-        <input type="text" placeholder="Enter Email"></input>
-        <input type="password" placeholder="Enter password"></input>
-        <input type="password" placeholder="Re-Enter password"></input>
+        <input type="text" name="name" value={user.name} placeholder="Enter Name" onChange={handleChange}></input>
+        <input type="text" name="email" value={user.email} placeholder="Enter Email" onChange={handleChange}></input>
+        <input type="password" name="password" value={user.password} placeholder="Enter Password" onChange={handleChange}></input>
+        <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Enter Password" onChange={handleChange}></input>
 
     <div className="button">Register</div>
     <div>or</div>
