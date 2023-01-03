4834907c46f174d73057ba76c49089618c8182eb
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2023-01-03 18:04:57 +0530

    backend started
    started working on backend
    connected front end
    created api(calls)

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+import express from "express"
+// const express = required ("express")
+import cors from "cors"
+import mongoose from "mongoose"
+
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+
+mongoose.connect('mongodb://localhost:27017/Login',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("DB COnnected")
+});
+
+//schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+//module
+const User = new mongoose.model("User",userSchema)
+
+
+
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+//Routes
+app.post("/login",(req, res)=> {
+    // console.log(req.body)
+    // res.send("my API Login")
+    const {name, email, password} = req.body
+    const user = new User({
+        name,
+        email,
+        password
+    })
+    user.save(err => {
+        if(err){
+            res.send(err)
+        } else {
+            res.send({message: "successfull register"})
+        }
+    })
+    
+})
+
+app.post("/Register",(req, res)=> {
+    res.send("my API Register")
+})
+
+app.listen(9002,()=>{
+    console.log("okay bro")
+})
+
+
--- src/Components/register/register.js
+++ src/Components/register/register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import "./register.css"
+import axios from "axios"
  const Register = () => {
 
   const [user, setUser] = useState({
@@ -17,6 +18,20 @@ import "./register.css"
       [name]: value
     })
   }
+
+  const registerUser = () => {
+    const { name, email, password, reEnterPassword } = user
+    if(name && email && password && (password === reEnterPassword)) {
+      alert("posted")
+    axios.post("http://localhost:9002/Register",{
+      user
+    }).then(res => console.log(res))
+  }
+  else{
+    alert("invalid input")
+  }
+  }
+
   return (
     <div className="register">
         <h1>Register</h1>
@@ -25,7 +40,7 @@ import "./register.css"
         <input type="password" name="password" value={user.password} placeholder="Enter Password" onChange={handleChange}></input>
         <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Enter Password" onChange={handleChange}></input>
 
-    <div className="button">Register</div>
+    <div className="button" onClick={registerUser}>Register</div>
     <div>or</div>
     <div className="button">Login</div>
     </div>
